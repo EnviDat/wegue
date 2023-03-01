453ed05accab29a6a39a62d399819389c6bab885
sam <sam.woodcock@protonmail.com>
2023-03-01 15:05:43 +0000

    ci: update eslint config and dev dep plugins, use prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,50 @@
 module.exports = {
   root: true,
+
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+    extraFileExtensions: [".vue"],
+  },
+
   env: {
-    node: true
+    browser: true,
+    node: true,
+    es2020: true,
   },
   extends: [
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential',
-    '@vue/standard'
+    "plugin:vue/essential",
+    "plugin:vue/strongly-recommended",
+    "prettier",
   ],
+  plugins: ["vue"],
   rules: {
     /* eslint-disable quote-props */
     // 'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
 
     // allow semicolons and the end of a statement
-    'semi': 'off',
+    semi: "off",
 
     // weaken some rules in development mode
-    'no-multiple-empty-lines': process.env.NODE_ENV === 'development' ? 'warn' : 'error',
-    'no-unused-vars': process.env.NODE_ENV === 'development' ? 'warn' : 'error',
-    'space-before-blocks': process.env.NODE_ENV === 'development' ? 'warn' : 'error',
-    'space-before-function-paren': process.env.NODE_ENV === 'development' ? 'warn' : 'error',
-    'object-curly-spacing': process.env.NODE_ENV === 'development' ? 'warn' : 'error',
-    'indent': process.env.NODE_ENV === 'development' ? 'warn' : 'error',
-    'comma-dangle': process.env.NODE_ENV === 'development' ? 'warn' : 'error',
-    'comma-spacing': process.env.NODE_ENV === 'development' ? 'warn' : 'error',
-    'quotes': process.env.NODE_ENV === 'development' ? 'warn' : 'error',
-    'padded-blocks': process.env.NODE_ENV === 'development' ? 'warn' : 'error',
-    'keyword-spacing': process.env.NODE_ENV === 'development' ? 'warn' : 'error',
-    'arrow-spacing': process.env.NODE_ENV === 'development' ? 'warn' : 'error'
+    "no-multiple-empty-lines":
+      process.env.NODE_ENV === "development" ? "warn" : "error",
+    "no-unused-vars": process.env.NODE_ENV === "development" ? "warn" : "error",
+    "keyword-spacing":
+      process.env.NODE_ENV === "development" ? "warn" : "error",
+    "arrow-spacing": process.env.NODE_ENV === "development" ? "warn" : "error",
     /* eslint-enable quote-props */
   },
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
+
   overrides: [
     {
       files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+        "**/__tests__/*.{j,t}s?(x)",
+        "**/tests/unit/**/*.spec.{j,t}s?(x)",
       ],
       env: {
-        mocha: true
-      }
-    }
-  ]
-}
+        mocha: true,
+      },
+    },
+  ],
+};
